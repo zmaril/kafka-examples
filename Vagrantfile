ed2ef25baafd2bcf9351022576ea5c8b979a3cda
Zack Maril <zmaril@sunlightfoundation.com>
2015-05-04 10:20:44 -0400

    Simple toy example done
    * Switched to debian, solved some fiddley postgres things
    * Learned how to use psycopg2 to actually do thing

--- Vagrantfile
+++ Vagrantfile
@@ -7,8 +7,8 @@ SSH_PORT = 2232
 Vagrant.require_version ">= 1.5.0"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-    config.vm.box = "chef/ubuntu-14.04"
-
+    #config.vm.box = "chef/ubuntu-14.04"
+    config.vm.box = "chef/debian-7.8" 
     # awkward fix for SSH reassignment issue (re-evaluate w/ Vagrant 1.5.4)
     config.vm.network :forwarded_port, guest: 22, host: 2222, id: "ssh", disabled: true
     config.vm.network :forwarded_port, guest: 22, host: SSH_PORT, auto_correct: true
--- scripts/consume.py
+++ scripts/consume.py
@@ -1,8 +1,29 @@
 from kafka import SimpleConsumer, KafkaClient
-
+import psycopg2 
 kafka = KafkaClient("10.42.2.106:9092")
 consumer = SimpleConsumer(kafka,"test-group","updates")
+
+conn = psycopg2.connect("dbname=postgres user=postgres")
+curs = conn.cursor()
+
+try:    
+    curs.execute("CREATE TABLE props (id serial PRIMARY KEY, prop varchar(10))")
+    curs.commit()
+except Exception:
+    print "Table already created, roll 'em on back'"
+    conn.rollback()
+
+try:
+    curs.execute("INSERT INTO props (id,prop) values (1,'')")
+    conn.commit()
+except Exception:
+    print "Test row already created, roll it on back'"
+    conn.rollback()
+    
 kafka.ensure_topic_exists('updates')
 
-for message in consumer:
-    print(message)
+for om in consumer:
+    prop = eval(om.message.value)
+    print(prop[1])
+    curs.execute("UPDATE props SET prop = '{0}' where id = 1".format(prop[1]))
+    conn.commit()
